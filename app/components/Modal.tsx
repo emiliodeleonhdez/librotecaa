fa5c956e119e560e8154fa7942ebe8510bbdc30a
emiliodeleon <emiliodeleon.contact@gmail.com>
2024-07-15 19:10:25 -0600

    responsive modal

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -27,19 +27,19 @@ const BookModal: React.FC<BookModalProps> = ({
         >     
           <section className="flex flex-col justify-center items-center">
             <div
-              className="bg-white p-4 rounded-lg shadow-lg flex flex-col w-[600px]"
+              className="bg-white p-4 rounded-lg shadow-lg flex flex-col w-64 md:w-1/2 lg:w-[600px]"
               onClick={(e) => e.stopPropagation()}
             >
-              <div className="flex">
+              <div className="flex flex-col md:flex-row items-center">
+              <p className="font-bold text-xl hover:text-sky-800 cursor-pointer" onClick={onCloseModal}>x</p>
                 <div className="w-20 m-2">
                   <img className="object-contain" src={coverImage} alt="book" />
                 </div>
-                <div className="flex flex-col p-2 flex-grow">
+                <div className="flex flex-col p-2 items-center md:items-start justify-center">
                   <h2 className="text-2xl font-bold">{title}</h2>
                   <p>{author}</p>
                   <p className="py-3">{synopsis}</p>
                 </div>
-                <p className="font-bold text-xl hover:text-sky-800 cursor-pointer" onClick={onCloseModal}>x</p>
               </div>
               <div className="flex justify-center mt-4">
                 <AppButton styles="w-60 hover:border-green-500 hover:text-emerald-900	 border-black border-2 p-4 rounded-full" onClick={()=> console.log("confirmar")} text={"Confirmar Apartado"}/>
