f72af77c9b89334aa1558f76832fbaa87fdfbdcc
emiliodeleon <emiliodeleon.contact@gmail.com>
2024-07-15 20:29:04 -0600

    added key

--- app/components/Aside.tsx
+++ app/components/Aside.tsx
@@ -10,8 +10,8 @@ const AppAside: React.FC<AppAsideProps> = ({ options }) => {
     <aside className="bg-gray-200 w-full md:w-1/4 lg:w-1/5 p-4 hidden md:block">
       <nav>
         <ul>
-          {options.map((item) => (
-            <li className="mb-2">
+          {options.map((item, index) => (
+            <li key={index + item.href} className="mb-2">
               <a href={item.href} className="text-blue-700">
                 {item.text}
               </a>
