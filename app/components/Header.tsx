dc3b967e8bfc4286550c48d0dd402df937a17a3a
emiliodeleon <emiliodeleon.contact@gmail.com>
2024-07-15 22:26:13 -0600

    add landing and delete console log

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -12,15 +12,21 @@ const Header: React.FC<HeaderProps> = ({ title }) => {
   const [showMenu, setShowMenu] = useState<boolean>(false);
 
   const handleShowMenu = () => {
-    console.log("click");
     setShowMenu(!showMenu);
   };
 
   return (
     <header className="bg-blue-600 text-white p-4 flex items-center gap-3 sticky top-0 z-50">
-      <GiHamburgerMenu onClick={handleShowMenu} className="cursor-pointer md:hidden" />
+      <GiHamburgerMenu
+        onClick={handleShowMenu}
+        className="cursor-pointer md:hidden"
+      />
       <h1 className="text-2xl">{title}</h1>
-      <AppAsideBurguer options={asideLinks} show={showMenu} onClose={handleShowMenu} />
+      <AppAsideBurguer
+        options={asideLinks}
+        show={showMenu}
+        onClose={handleShowMenu}
+      />
     </header>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      <h1>Home</h1>
+      <h1>Libroteca</h1>
+      <img src={"/landing_main.png"}/>
     </main>
   );
 }
